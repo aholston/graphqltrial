1317d5448c9e01a73d1e20d700129f2fb58f1022
Andre <andreholston@Andres-MBP-2.lan>
2022-03-24 20:36:44 -0500

    Add in external mock api connection using json server package

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,12 @@
-const graphQL require('graphql');
+const graphQL = require('graphql');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
-} = graphql;
+  GraphQLInt,
+  GraphQLSchema
+} = graphQL;
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -14,3 +16,22 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+    type: UserType,
+    args: {
+      id: { type: GraphQLString }
+    },
+    resolve(parentValue, args) {
+      return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(res => res.data)
+    }
+  }}
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
